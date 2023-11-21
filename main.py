139e153fb4f2d6d4ee78357130b2c41bbac33d0e
saxum <saxum87@gmail.com>
2023-11-21 04:39:58 +0000

    small program to convert days to hours

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ calculation_to_units = 24
 
 
 def days_to_units(num_of_days):
-    if num_of_days > 0:
-        return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
-    elif num_of_days == 0:
-        return "you entered a 0, please enter a valid positive number"
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
 
 
 def validate_and_execute():
     if user_input.isdigit():
         user_input_number = int(user_input)
-        calculated_value = days_to_units(user_input_number)
-        print(calculated_value)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a 0, please enter a valid positive number")
     else:
         print("your input is not a valid number.")
 
