f7b41227e1894171528fea3588949b47892305df
saxum <saxum87@gmail.com>
2023-11-21 08:13:42 +0000

    using datetime module to do calculations

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -5,7 +5,9 @@ input_list = user_input.split(":")
 
 goal = input_list[0]
 deadline = input_list[1]
+deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+today_date = datetime.datetime.today()
+time_till = deadline_date - today_date
 
-datetime.datetime
+print(f"Dear user! Time remaining for your goal: {goal} is {time_till}")
 
-print(input_list)
