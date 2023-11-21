6e1b90935855dcf1e4418bdfd6578ac1792335cf
saxum <saxum87@gmail.com>
2023-11-21 07:12:08 +0000

    using dictionary to pass also unit to switch

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ user_input = ""
 while user_input != "exit":
     user_input = input("Hey user, enter a number of days and conversion unit!\n")
     days_and_unit = user_input.split(":")
-    print(days_and_unit)
+    # print(days_and_unit)
     days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
-    print(days_and_unit_dictionary)
+    # print(days_and_unit_dictionary)
 
     validate_and_execute()
