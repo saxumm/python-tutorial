e61ac19bf120c44dbe0463080981ecd7e8a52b52
saxum <saxum87@gmail.com>
2023-11-21 05:04:19 +0000

    added exit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ calculation_to_units = 24
 
 
 def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}\n"
 
 
 def validate_and_execute():
@@ -16,12 +16,14 @@ def validate_and_execute():
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
-            print("you entered a 0, please enter a valid positive number")
+            print("you entered a 0, please enter a valid positive number\n")
         else:
-            print("you entered a negative number, please enter a valid positive number")
+            print("you entered a negative number, please enter a valid positive number\n")
     except ValueError:
-        print("your input is not a valid number.")
+        print("your input is not a valid number.\n")
 
 
-user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
-validate_and_execute()
+user_input = ""
+while user_input != "exit":
+    user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
+    validate_and_execute()
