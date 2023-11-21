4c4d60dd030d297e87416189f29d8b37984f956e
saxum <saxum87@gmail.com>
2023-11-21 07:21:50 +0000

    move functions to helper file and import them to main file

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-
-def days_to_units(num_of_days, conversion_unit):
-    if conversion_unit == "hours":
-        return f"{num_of_days} days are {num_of_days * 24} hours\n"
-    elif conversion_unit == "minutes":
-        return f"{num_of_days} days are {num_of_days * 24 * 60} minutes\n"
-    else:
-        return "unsupported unit"
-
-
-def validate_and_execute():
-    try:
-        user_input_number = int(days_and_unit_dictionary["days"])
-
-        # conversion only for positive numbers
-        if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
-            print(calculated_value)
-        elif user_input_number == 0:
-            print("you entered a 0, please enter a valid positive number\n")
-        else:
-            print("you entered a negative number, please enter a valid positive number\n")
-    except ValueError:
-        print("your input is not a valid number.\n")
-
-
+import helper
 user_input = ""
 while user_input != "exit":
     user_input = input("Hey user, enter a number of days and conversion unit!\n")
@@ -32,4 +7,4 @@ while user_input != "exit":
     days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
     # print(days_and_unit_dictionary)
 
-    validate_and_execute()
+    helper.validate_and_execute(days_and_unit_dictionary)
