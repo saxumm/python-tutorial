ee82072abdf7bb7f20b8eddfd76a476691e6aa10
saxum <saxum87@gmail.com>
2023-11-22 05:21:20 +0000

    new post

--- class-and-objects/main.py
+++ class-and-objects/main.py
@@ -1,6 +1,10 @@
 from user import User
+from post import Post
 app_user_one = User("nn@nn.com", "Nana Janashia", "pwd1", "DevOps Engineer")
 app_user_one.get_user_info()
 
 app_user_two = User("aa@aa.com", "James Bond", "goldeneye", "007")
 app_user_two.get_user_info()
+
+newpost = Post("on a secret miwsion", app_user_two.name)
+newpost.get_post_info()
