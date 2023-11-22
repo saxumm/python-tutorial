28e961f01dea098615448c8f8fb8219fc548f7e8
saxum <saxum87@gmail.com>
2023-11-22 05:07:41 +0000

    defining User class

--- class-and-objects/user.py
+++ class-and-objects/user.py
@@ -16,3 +16,6 @@ class User:
 
 app_user_one = User("nn@nn.com", "Nana Janashia", "pwd1", "DevOps Engineer")
 app_user_one.get_user_info()
+
+app_user_two = User("aa@aa.com", "James Bond", "goldeneye", "007")
+app_user_two.get_user_info()
