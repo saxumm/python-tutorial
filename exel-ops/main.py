5e2e1e84e9fd6f317b1df822353fe1289ea60c08
saxum <saxum87@gmail.com>
2023-11-22 04:32:55 +0000

    operations on exel file

--- exel-ops/main.py
+++ exel-ops/main.py
@@ -12,7 +12,7 @@ for product_row in range(2, product_list.max_row + 1):
     inventory = product_list.cell(product_row, 2).value
     price = product_list.cell(product_row, 3).value
     product_num = product_list.cell(product_row, 1).value
-
+    inventory_price = product_list.cell(product_row, 5)
     # calculation number of products per supplier
     if supplier_name in products_per_supplier:
         current_num_products = products_per_supplier.get(supplier_name)
@@ -31,4 +31,12 @@ for product_row in range(2, product_list.max_row + 1):
     if inventory < 10:
         product_under_10_inv[int(product_num)] = int(inventory)
 
+    # add value for total inventory price
+    inventory_price.value = inventory * price
+
+
+print(products_per_supplier)
+print(total_value_per_supplier)
 print(product_under_10_inv)
+
+inv_file.save("inventory_with_total_value.xlsx")
