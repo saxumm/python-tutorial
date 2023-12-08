cc7afe879ab38f82f9313e04df8c177296244258
saxum <saxum87@gmail.com>
2023-12-08 08:14:29 +0000

    cs50 harvard course hello

--- Harvard-CS50’s-Introduction-to-Programming-with-Python/hello/main.py
+++ Harvard-CS50’s-Introduction-to-Programming-with-Python/hello/main.py
@@ -1,5 +1,9 @@
 # Ask user for their name
 name = input("What's your name? ")
 
+
+# Remove whitespace  from str and capitalize
+name = name.strip().title()
+
 # Say hello to user
 print(f"Hello, {name}")
